c13bc7aeb7c0d31b9e18f90ac744cbba9fabe489
Andrew Duthie <andrew@andrewduthie.com>
2018-10-12 11:50:30 -0400

    Tooling: Improve git diff processing comments

--- bin/process-git-diff.js
+++ bin/process-git-diff.js
@@ -8,10 +8,27 @@
 //
 // git diff -U0 | xargs -0 node bin/process-git-diff
 
+// Example input:
+//
+// diff --git a/package-lock.json b/package-lock.json
+// index e8c8a25dc..251af8689 100644
+// --- a/package-lock.json
+// +++ b/package-lock.json
+// @@ -14373 +14373,2 @@
+// -                       "dev": true
+// +                       "dev": true,
+// +                       "optional": true
+// @@ -14648 +14649,2 @@
+// -                       "dev": true
+// +                       "dev": true,
+// +                       "optional": true
+
 const hasNonOptionalDiff = !! ( process.argv[ 2 ] || '' )
+	// Strip individual diffs of optional-only.
 	.replace( /@@ -\d+ \+\d+,\d+ @@\n-.+\n\+.+,\n\+.+\"optional\": true\n/gm, '' )
+	// If no more line diffs remain after above, remove diff heading for file.
 	.replace( /diff --git a\/package-lock.json b\/package-lock.json\nindex \w+..\w+ \d+\n--- a\/package-lock.json\n\+\+\+ b\/package-lock.json\n(?!@@)/, '' );
 
-// Exit with error code if, after replace, changes still exist.
 
+// Exit with error code if, after replace, changes still exist.
 process.exit( hasNonOptionalDiff ? 1 : 0 );
